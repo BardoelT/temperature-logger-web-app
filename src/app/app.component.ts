9c977a7613ea7cd45db3188cf557ffefd5935675
Thijs Bardoel <thijs_bardoel@hotmail.com>
2019-08-15 14:23:21 +0200

    the table is now properly shown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
   constructor(private database: DatabaseService, private breakpointObserer: BreakpointObserver) { }
 
   myData = [];
-  columnNames = ["", 'load1', 'load5', 'load15'];
+  columnNames = [];
   myType = "LineChart"
   myOptions = {
     hAxis: {
@@ -25,8 +25,9 @@ export class AppComponent implements OnInit {
   }
 
   formatData(data: any): void {
-    console.log(data);
-    this.myData = data.map((record: any[]) => {
+    console.log(data.values);
+    this.columnNames = data.columns;
+    this.myData = data.values.map((record: any[]) => {
       record[0] = new Date(Date.parse(record[0]));
       return record;
     });
@@ -38,12 +39,23 @@ export class AppComponent implements OnInit {
     this.database.setDataFields(['load1', 'load5', 'load15']);
     this.database.getDataLastHour().subscribe(data => this.formatData(data));
   }
+
+  func(data: any): void {
+    this.myData = data.values[0].map(record => {
+        return [`${data.columns.shift()}`, record];
+    });
+    this.myData.shift();
+  }
   
   updateQuery(opt: string): void {
     console.log(opt);
+    this.myData = [];
+
     if (opt === "Current") {
       this.myType = "Table";
-      this.database.getDataCurrent().subscribe(data => this.formatData(data));
+      this.columnNames = [ "Sensor", "Temperature" ];
+      this.myData = [];
+      this.database.getDataCurrent().subscribe(data => this.func(data));
       return;
     }
 
